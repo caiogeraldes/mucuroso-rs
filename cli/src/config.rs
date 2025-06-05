a5812595c4a3165fb9a67b4fb9a99fa5bcb034ad
Caio Geraldes <caio.geraldes@usp.br>
2025-06-05 14:00:20 -0300

    comp stuff

---
+++ cli/src/config.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct Config {
+    pub(crate) db_file: PathBuf,
+}
+
+/// `MyConfig` implements `Default`
+impl ::std::default::Default for Config {
+    fn default() -> Self {
+        let db_file = if let Some(mut home_dir) = std::env::home_dir() {
+            home_dir.push(".mucuroso/mucuroso.db");
+            home_dir
+        } else {
+            panic!("Eita")
+        };
+        Self { db_file }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::home_dir;
+
+    use super::*;
+    use toml;
+
+    #[test]
+    fn config_tester() {
+        let config: Config = toml::from_str(
+            r#"
+                db_file = '/home/teste/teste.db'
+            "#,
+        )
+        .unwrap();
+        assert_eq!(config.db_file, PathBuf::from("/home/teste/teste.db"));
+
+        let mut path = home_dir().unwrap();
+        path.push(".mucuroso/mucuroso.db");
+
+        let cfg: Config = confy::load("mucuroso", "config").unwrap();
+        assert_eq!(cfg.db_file, path);
+    }
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,82 @@
+use clap::{Parser, Subcommand};
+use garmin_gym_fit_parser::session::SessionData;
+use rustbreak::FileDatabase;
+use rustbreak::deser::Ron;
+use std::{fs::File, path::PathBuf};
+
+use crate::config::Config;
+
+mod config;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initializes a data repository
+    Init {
+        /// Fit File containing the first register in the DataBase
+        fit_file: PathBuf,
+    },
+    /// Checks if fit file is parsable
+    Add {
+        /// Fit file
+        fit_file: PathBuf,
+    },
+    /// Checks if fit file is parsable
+    Check {
+        /// Fit file
+        fit_file: PathBuf,
+    },
+    /// Lists all sessions in data base
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let config: Config = confy::load("mucuroso", "config").unwrap();
+
+    match &cli.command {
+        Commands::Init { fit_file } => {
+            let mut fp = File::open(&fit_file).unwrap();
+            let db = FileDatabase::<Vec<SessionData>, Ron>::create_at_path(
+                config.db_file,
+                vec![SessionData::try_from_reader(&mut fp).unwrap()],
+            )
+            .unwrap();
+            db.save().unwrap();
+        }
+        Commands::Add { fit_file } => {
+            let mut fp = File::open(&fit_file).unwrap();
+            let session_data = SessionData::try_from_reader(&mut fp).unwrap();
+
+            let db = FileDatabase::<Vec<SessionData>, Ron>::load_from_path(config.db_file).unwrap();
+            db.write(|db| {
+                db.push(session_data);
+                db.dedup();
+                db.sort();
+            })
+            .unwrap();
+            db.save().unwrap();
+        }
+        Commands::Check { fit_file } => {
+            let mut fp = File::open(&fit_file).unwrap();
+            match SessionData::try_from_reader(&mut fp) {
+                Ok(_) => {
+                    println!("File {:#?} is parsable", fit_file);
+                    std::process::exit(exitcode::OK);
+                }
+                Err(e) => {
+                    eprintln!("Error: {e}");
+                    std::process::exit(1);
+                }
+            }
+        }
+        Commands::List => todo!(),
+    }
 }
